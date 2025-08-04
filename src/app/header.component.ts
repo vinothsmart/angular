8f50d9c6278630bbda6c03de80fd2ab2f440d540
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-04 17:13:01 +0530

    header comp with styles

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -4,5 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {}
