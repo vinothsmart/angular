eaf343db973b5d10b680121a7b197dd08cd012dc
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-04 10:49:17 +0530

    updated statrting project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'first-angular-app';
-}
+export class AppComponent {}
