8352fb7f59012d11637d52f4d59fd548d25841cf
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-04 19:48:17 +0530

    pending

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -3,10 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
-export class UserComponent {
-
-}
+export class UserComponent {}
