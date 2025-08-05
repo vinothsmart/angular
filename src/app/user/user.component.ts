04e4b61634a21695f32cd3dcc31d7504c053e28a
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-05 13:02:00 +0530

    using signals

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = () => Math.floor(Math.random() * DUMMY_USERS.length);
@@ -9,12 +9,14 @@ const randomIndex = () => Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex()];
-  get imagePath() {
-    return `assets/users/${this.selectedUser.avatar}`;
-  }
+  selectedUser = signal(DUMMY_USERS[randomIndex()]);
+  imagePath = computed(() => `assets/users/${this.selectedUser().avatar}`);
+
+  // get imagePath() {
+  //   return `assets/users/${this.selectedUser.avatar}`;
+  // }
+
   onUserClick() {
-    this.selectedUser = DUMMY_USERS[randomIndex()];
-    console.log('User changed to:', this.selectedUser.name);
+    this.selectedUser.set(DUMMY_USERS[randomIndex()]);
   }
 }
