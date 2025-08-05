23927a8ee6327e88f0810266193decb57d7f1856
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-05 14:59:52 +0530

    list all by json

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = () => Math.floor(Math.random() * DUMMY_USERS.length);
@@ -9,14 +9,14 @@ const randomIndex = () => Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex()]);
-  imagePath = computed(() => `assets/users/${this.selectedUser().avatar}`);
+  @Input() avatar!: string;
+  @Input() name!: string;
 
-  // get imagePath() {
-  //   return `assets/users/${this.selectedUser.avatar}`;
-  // }
+  get imagePath() {
+    return 'assets/users/' + this.avatar || DUMMY_USERS[randomIndex()].avatar;
+  }
 
   onUserClick() {
-    this.selectedUser.set(DUMMY_USERS[randomIndex()]);
+    console.log(`User clicked: ${this.name}`);
   }
 }
