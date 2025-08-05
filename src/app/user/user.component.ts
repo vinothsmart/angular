e8a01026ea43b9879d6e68897cb36905e5e064d2
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-05 16:26:14 +0530

    using output

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = () => Math.floor(Math.random() * DUMMY_USERS.length);
@@ -12,17 +12,23 @@ export class UserComponent {
   @Input({
     required: true,
   })
+  id!: string;
+  @Input({
+    required: true,
+  })
   avatar!: string;
   @Input({
     required: true,
   })
   name!: string;
 
+  @Output() select = new EventEmitter();
+
   get imagePath() {
     return 'assets/users/' + this.avatar || DUMMY_USERS[randomIndex()].avatar;
   }
 
   onUserClick() {
-    console.log(`User clicked: ${this.name}`);
+    this.select.emit(this.id);
   }
 }
