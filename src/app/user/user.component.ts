2f556ce3231de93c11d73ae62c927647e162802a
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-05 15:03:13 +0530

    required usage

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,8 +9,14 @@ const randomIndex = () => Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({
+    required: true,
+  })
+  avatar!: string;
+  @Input({
+    required: true,
+  })
+  name!: string;
 
   get imagePath() {
     return 'assets/users/' + this.avatar || DUMMY_USERS[randomIndex()].avatar;
