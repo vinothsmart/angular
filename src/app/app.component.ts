693ff5a28301838fd13a2d1479cd62c2edca21cf
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-06 18:56:05 +0530

    over riding the value OnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +7,13 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   user: any = null;
+
+  ngOnInit() {
+    this.user = {
+      name: 'Vinoth Kanna',
+      age: 35,
+    };
+  }
 }
