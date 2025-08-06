b1ecae14e9ba2cdcb15728baa090824810e567d5
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-06 19:40:07 +0530

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CounterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'counter-app';
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-counter',
   imports: [],
   templateUrl: './counter.component.html',
-  styleUrl: './counter.component.css'
+  styleUrl: './counter.component.css',
 })
 export class CounterComponent {
-
+  count = 0;
 }
