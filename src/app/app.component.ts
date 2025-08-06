2f922f382afd34be342bb18e10752fc9a8353f60
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-06 17:30:26 +0530

    two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Homes';
+  fullName = 'Sachin Tendulkar';
 }
