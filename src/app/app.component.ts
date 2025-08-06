5ac8473c7a4800db71b409963a970e25cc197448
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-06 16:33:09 +0530

    template up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,9 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // templateUrl: './app.component.html',
+  template: `<h1>Welcome to Angular!</h1>`,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'first-angular-app';
