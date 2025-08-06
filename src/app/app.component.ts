c4e81e46fda9cf8c6bd54e117bd76f8b512ef0d6
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-06 19:59:31 +0530

    ng for directive not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CounterComponent } from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CounterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'counter-app';
+  movies = ['Inception', 'Interstellar', 'The Dark Knight'];
 }
