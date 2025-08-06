7f45a416bba9b2bd11666e9e2d9a34336e3213af
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-06 20:16:39 +0530

    first and last

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  movies = ['Inception', 'Interstellar', 'The Dark Knight'];
+  fruits = ['Apple', 'Banana', 'Cherry'];
 }
