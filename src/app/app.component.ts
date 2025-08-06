4fe25c80de8daa6d5d3a08844b75b66b96e1e53d
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-06 17:31:04 +0530

    String Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  fullName = 'Sachin Tendulkar';
+  title = 'Welcome to My Angular App';
 }
