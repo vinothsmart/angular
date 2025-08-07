cffc650c46ac457085f0b9127bf0f867f4f19a2e
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-07 10:14:21 +0530

    using propert ngif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry', 'Fig', 'Grape'];
+  showMe = true;
 }
