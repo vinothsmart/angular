59f0d7c5c75fb86f64228e709e70a39a01bd704e
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-07 19:00:16 +0530

    date pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DemoComponent } from './demo/demo.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DemoComponent],
+  imports: [RouterOutlet, DemoComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'augular-directive';
+  toDate = new Date();
 }
