fef4611026fbb1b87a164bf3531990ea70bdd820
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-07 18:35:48 +0530

    directive updated

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appHighlight]'
+  selector: '[appHighlight]',
 })
 export class HighlightDirective {
+  @Input() appHighlight = '';
 
-  constructor() { }
+  constructor(private el: ElementRef) {}
 
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight(this.appHighlight || 'yellow');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
 }
