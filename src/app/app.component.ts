8aa46027d2d0cbaa75f72dbb0bab7b713ef0dbff
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-07 10:39:59 +0530

    show and hide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  showMe = true;
+  showMe = false;
 }
