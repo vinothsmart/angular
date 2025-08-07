cf70d93c9249a69ecb2dc3856bb1685b1195719e
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-07 11:10:19 +0530

    using two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,6 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  applyRed = false;
+}
