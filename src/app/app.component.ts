4dd7209724c5fdcf856b6caced3c45762ac12f11
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-08 18:13:37 +0530

    list all cars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormComponent } from './form/form.component';
+import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, FormComponent],
+  imports: [RouterOutlet, CommonModule, FormComponent, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,11 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
 export class ListComponent {
-
+  carList: string[] = [];
 }
