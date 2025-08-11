3e5533c14388e7cc1d53dea24cd0c521c192edf8
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-11 12:36:55 +0530

    removing unused imports and template elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { FormComponent } from './form/form.component';
-import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, FormComponent, ListComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  // carList: string[] = [];
-  // onCarAdded(carName: string) {
-  //   this.carList.push(carName);
-  // }
-}
+export class AppComponent {}
