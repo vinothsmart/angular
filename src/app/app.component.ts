24d2a20bf5351babd04a83a7886a3393e15e140c
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-12 10:33:32 +0530

    ngAfterViewInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SimpleMessageComponent } from './simple-message/simple-message.component';
 import { RouterModule } from '@angular/router';
+import { HelloComponent } from './hello/hello.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, FormsModule, SimpleMessageComponent],
+  imports: [RouterModule, FormsModule, HelloComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
   imports: [],
   templateUrl: './hello.component.html',
-  styleUrl: './hello.component.css'
+  styleUrl: './hello.component.css',
 })
-export class HelloComponent {
+export class HelloComponent implements AfterViewInit {
+  // focus on the input field
+  @ViewChild('nameInput') nameInput!: ElementRef;
 
+  ngAfterViewInit() {
+    this.nameInput.nativeElement.focus();
+    console.log('Input focused');
+  }
 }
