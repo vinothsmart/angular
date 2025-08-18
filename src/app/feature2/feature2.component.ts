7c4829be2282cf076599bbcd723a165f774a4915
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-18 21:37:37 +0530

    ng g c feature2

---
+++ src/app/feature2/feature2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature2',
+  imports: [],
+  templateUrl: './feature2.component.html',
+  styleUrl: './feature2.component.css'
+})
+export class Feature2Component {
+
+}
