c1a6ef00be02d9b3cfa99eab55fb4de9aa206482
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-18 21:34:19 +0530

    ng g c feature1

---
+++ src/app/feature1/feature1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature1',
+  imports: [],
+  templateUrl: './feature1.component.html',
+  styleUrl: './feature1.component.css'
+})
+export class Feature1Component {
+
+}
