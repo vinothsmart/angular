d2da188a499836d1f47499d1272de9c93a0190f3
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-18 21:24:28 +0530

    Programatic Navigation Link

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  constructor(private router: Router) {}
+  navigateToAbout() {
+    // Logic to navigate to the About page
+    // This could be implemented using Angular's Router service
+    this.router.navigate(['/about']);
+  }
 }
